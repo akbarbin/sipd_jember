516f9f747781940097d43265899b11e126f22893
Mahendri Winata <mahen.0112@gmail.com>
2013-05-21 20:37:11 +0700

    Config application

--- application/config/database.php
+++ application/config/database.php
@@ -52,9 +52,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
+$db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['database'] = 'sipd_jember';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
