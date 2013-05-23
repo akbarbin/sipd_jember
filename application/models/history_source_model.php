fd314f2514a62d23f495e72455c8e78b1665e097
Mahendri Winata <mahen.0112@gmail.com>
2013-05-24 06:38:20 +0700

    #add controller and model history

---
+++ application/models/history_source_model.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * @author Mahendri Winata <mahen.0112@gmail.com>
+ */
+
+class History_model extends App_Model {
+
+  var $table = 'histories';
+
+  function __construct() {
+    parent::__construct();
+  }
+
+}
+
+?>
