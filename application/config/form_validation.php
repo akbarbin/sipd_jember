dbb180f2c30b15fd4e25e1ac484d61eaf8e0b272
Mahendri Winata <mahen.0112@gmail.com>
2013-05-24 08:45:43 +0700

    add validation submit user edit account

---
+++ application/config/form_validation.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+    'user/edit_account' => array(
+        array(
+            'field' => 'name',
+            'label' => 'Nama',
+            'rules' => 'required|alpha'
+        ),
+    )
+);
+?>
--- application/controllers/admin/user.php
+++ application/controllers/admin/user.php
@@ -7,20 +7,25 @@ if (!defined('BASEPATH'))
  * @author Mahendri Winata <mahen.0112@gmail.com>
  */
 class User extends Admin_Controller {
-    
+
   /**
    * @author Mahendri Winata <mahen.0112@gmail.com>
    * 
    * Description :
    * This function load admin user edit account page.
    */
-  public function edit_account(){
-    $user = $this->db->get_where('users', array('username' => self::$sessionLogin['username']))->result();
-    $data['user'] = $user[0];
-    $data['title'] = 'Edit User Account';
-    $data['content'] = 'user/edit_account';
-    $this->load->view('layout/admin', $data);
+  public function edit_account() {
+    $this->load->library('form_validation');
+    if ($this->form_validation->run()) {
+      print_r("Masuk");die;
+    } else {
+      $user = $this->db->get_where('users', array('username' => self::$sessionLogin['username']))->result();
+      $data['user'] = $user[0];
+      $data['title'] = 'Edit User Account';
+      $this->load->view('layout/admin', $data);
+    }
   }
 
 }
+
 ?>
