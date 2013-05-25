00beb8c05e1dfef2257adf1ff22fe29cca0ab43e
Mahendri Winata <mahen.0112@gmail.com>
2013-05-25 09:31:22 +0700

    add function error message after save, update or delete

--- application/core/App_Controller.php
+++ application/core/App_Controller.php
@@ -7,16 +7,47 @@ if (!defined('BASEPATH'))
  * App Controller use to add all function used by any user
  * @author Mahendri Winata <mahen.0112@gmail.com>
  */
-class App_Controller extends CI_Controller { 
-  
+class App_Controller extends CI_Controller {
+
   public static $sessionLogin;
-  
+
   public function __construct() {
     parent::__construct();
-    
+
     self::$sessionLogin = $this->session->all_userdata();
   }
+
+  /**
+   * @author Mahendri Winata <mahen.0112@gmail.com>
+   * 
+   * @param string $action
+   * @param boolean $callback_action
+   * @param string $message
+   */
+  protected function error_message($action = NULL, $callback_action = FALSE, $message = NULL) {
+    $actions = array(
+        'insert' => array(
+            TRUE => 'Anda berhasil melakukan penyimpanan data.',
+            FALSE => 'Anda gagal melakukan penyimpanan data'),
+        'update' => array(
+            TRUE => 'Anda berhasil melakukan perubahan data.',
+            FALSE => 'Anda gagal melakukan perubahan data.'),
+        'delete' => array(
+            TRUE => 'Anda berhasil menghapus data.',
+            FALSE => 'Anda gagal menghapus data.'),
+    );
+
+    $alert = array(
+        TRUE => 'success',
+        FALSE => 'error'
+    );
     
+    $message = (empty($message)) ? $actions[$action][$callback_action] : $message;
+    
+    $this->session->set_flashdata('message', array('alert' => $alert[$callback_action], 'message' => $message));
+
+  }
+
 }
 
 ?>
