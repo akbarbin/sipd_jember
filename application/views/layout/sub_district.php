f666f7b669b32c76653915c946d3b0d69ff40c63
Mahendri Winata <mahen.0112@gmail.com>
2013-05-30 08:40:27 +0700

    add html type and js for ie

--- application/views/layout/sub_district.php
+++ application/views/layout/sub_district.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
   <head>
     <meta charset="utf-8">
     <title><?php echo isset($title) ? 'SIPD Jember - ' . $title : 'SIPD Jember'; ?></title>
@@ -22,7 +25,7 @@
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
-    <?php echo js(array('html5shiv')) ?>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
     <!-- Fav and touch icons -->
