df05ae6f360541e385d4b35c6f589065c5334f79
Mahendri Winata <mahen.0112@gmail.com>
2013-05-30 08:09:28 +0700

    fix bug view data source

--- application/views/admin/data_source/view.php
+++ application/views/admin/data_source/view.php
@@ -2,10 +2,12 @@
 echo bootstrap_table_title($title);
 
 $data = array(
-    array('Nama', $unit->name),
-    array('Keterangan', $unit->description));
+    array('Nama', $data_source->name),
+    array('Telepon', $data_source->phone),
+    array('Kecamatan', $data_source->sub_district_name),
+    array('Keterangan', $data_source->address));
 echo bootstrap_table_view($data);
 
-echo anchor('admin/unit', 'Kembali', 'class="btn btn-danger btn-link-bootstrap"');
+echo anchor('admin/data_source', 'Kembali', 'class="btn btn-danger btn-link-bootstrap"');
 echo bootstrap_tag_close('div');
 ?>
