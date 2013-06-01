bdbedb3091912797530ed4b088ac1d56678458ec
Mahendri Winata <mahen.0112@gmail.com>
2013-06-01 19:13:59 +0700

    fix bug user index

--- application/views/admin/user/index.php
+++ application/views/admin/user/index.php
@@ -14,7 +14,12 @@ echo bootstrap_tag_open('tbody');
 $offset = (empty($offset)) ? 0 : $offset;
 foreach ($users as $key => $user) {
   $offset++;
-  echo bootstrap_table_tr(array($offset, $user->name, $user->phone, $user->email, $user->sub_district_name, $user->role_name, bootstrap_table_action($controller, $user->id)), 'td');
+  $actions = array(
+      'view' => array('name' => 'Detail', 'action' => 'view/' . $user->id),
+      'edit' => array('name' => 'Edit', 'action' => 'edit/' . $user->id),
+      'delete' => array('name' => 'Delete', 'action' => 'delete/' . $user->id),
+  );
+  echo bootstrap_table_tr(array($offset, $user->name, $user->phone, $user->email, $user->sub_district_name, $user->role_name, bootstrap_table_action_dropdown($controller, $actions)), 'td');
 }
 echo bootstrap_tag_close('tbody');
 echo bootstrap_tag_close('table');
