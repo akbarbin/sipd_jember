630022c2ca94be7f503c1525d07168049e5a6775
Mahendri Winata <mahen.0112@gmail.com>
2013-06-02 19:07:50 +0700

    style loading submit

--- webroot/js/custom-script.js
+++ webroot/js/custom-script.js
@@ -4,11 +4,8 @@ $(document).ready(function() {
 //    height: 250, // height not including margins, borders or padding 
 //  }); /*== make code pretty ==*/
 
-$('form').submit(function(){
-  $.colorbox({
-      rel : 'loading',
-      opocity : 0.6
-  });  
-});
+  $('form').submit(function() {
+    $.colorbox({opacity: 0.5, rel: 'loading', width: '100%', height: '100%'});
+  });
 
 });
