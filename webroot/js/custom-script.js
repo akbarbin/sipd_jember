aec31863c5f8c8e09b05dd0841bc6f28ec845a2c
Mahendri Winata <mahen.0112@gmail.com>
2013-06-08 00:06:40 +0700

    resolve and fixed bug #2- change colorbox function

--- webroot/js/custom-script.js
+++ webroot/js/custom-script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-//  $("#profile-editor").cleditor({
-//    width: 99 + '%', // width not including margins, borders or padding
-//    height: 250, // height not including margins, borders or padding 
-//  }); /*== make code pretty ==*/
-
   $('form').submit(function() {
-    $.colorbox({opacity: 0.5, rel: 'loading', width: '100%', height: '100%'});
+    $.colorbox({
+      opacity: 0.5, 
+      rel: 'loading', 
+      width: '100%', 
+      height: '100%',
+      overlayClose: false,
+      escKey: false,
+      close: ''
+    });
   });
 
 });
